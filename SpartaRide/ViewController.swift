6f070ebb2441d854b75fb16bb6398179bcad5a86
Brandon Bielicki <brandon@Brandons-Mini.attlocal.net>
2016-12-18 13:19:44 -0500

    Scroller select

--- SpartaRide/ViewController.swift
+++ SpartaRide/ViewController.swift
@@ -9,32 +9,38 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
     
     @IBOutlet weak var routeSelectButton: UIButton!
-    
     @IBOutlet weak var stopsToggleButton: UIButton!
     
     @IBOutlet weak var mapView: GMSMapView!
+    
+    @IBOutlet weak var pickerContainerView: UIView!
+    @IBOutlet weak var routePickerView: UIPickerView!
+    
+    let routeNumbers = ["01","02","03","05","07","08","09","10","11","12","13","14","15","16","20","22","23","24","25","26","30","31","32","33","34","35","36","39","46","48"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let camera = GMSCameraPosition.camera(withLatitude: 42.7369792, longitude: -84.48386540000001, zoom: 15.0)
-        //let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        //mapView.isMyLocationEnabled = true
-        //view = mapView
         
         mapView.isMyLocationEnabled = true
         mapView.camera = camera
         
         routeSelectButton.layer.cornerRadius = 0.5 * routeSelectButton.bounds.size.width
         routeSelectButton.clipsToBounds = true
-        view.addSubview(routeSelectButton)
         
         stopsToggleButton.layer.cornerRadius = 0.5 * stopsToggleButton.bounds.size.width
         stopsToggleButton.clipsToBounds = true
-        view.addSubview(stopsToggleButton)
+        
+        pickerContainerView.layer.cornerRadius = 0.5 * pickerContainerView.bounds.size.width
+        pickerContainerView.clipsToBounds = true
+        
+        self.routePickerView.delegate = self
+        self.routePickerView.dataSource = self
+        pickerContainerView.isHidden = true
         
     }
     
@@ -44,10 +50,7 @@ class ViewController: UIViewController {
     
     @IBAction func routeSelectButtonClick(_ sender: Any) {
         print("ROUTE SELECT")
-    }
-    
-    override func viewDidLayoutSubviews() {
-        
+        pickerContainerView.isHidden = false
     }
     
     override func didReceiveMemoryWarning() {
@@ -55,6 +58,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return routeNumbers.count
+    }
+    //MARK: Delegates
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return routeNumbers[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        routeSelectButton.setTitle(routeNumbers[row], for: .normal)
+        pickerContainerView.isHidden = true
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let titleData = routeNumbers[row]
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSFontAttributeName:UIFont.boldSystemFont(ofSize: 35.0),NSForegroundColorAttributeName:UIColor.white])
+        return myTitle
+    }
 
 }
 
